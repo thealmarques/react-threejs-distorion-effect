4d34dd6f95d718bde62fef8c220c43593054760d
Andre Marques <utilizador@MBPdeUtilizador.Home>
2020-11-13 14:05:43 +0000

    Removed unused dependency

--- src/components/landing/landing.tsx
+++ src/components/landing/landing.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './landing.scss';
 import { Animal } from '../../interfaces/animals';
 import { SliderEffect } from '../../utils/effect';
